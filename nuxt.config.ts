37679b8ef69202b1b4297e245310e397378766eb
Sébastien Chopin <atinux@gmail.com>
2025-01-10 11:42:27 +0100

    chore: update deps

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,22 +9,9 @@ export default defineNuxtConfig({
   // https://devtools.nuxt.com
   devtools: { enabled: true },
 
-  // Env variables - https://nuxt.com/docs/getting-started/configuration#environment-variables-and-private-tokens
-  runtimeConfig: {
-    public: {
-      // Can be overridden by NUXT_PUBLIC_HELLO_TEXT environment variable
-      helloText: 'Hello from the Edge 👋',
-    },
-  },
   // https://nuxt.com/docs/getting-started/upgrade#testing-nuxt-4
   future: { compatibilityVersion: 4 },
   compatibilityDate: '2024-07-30',
-
-  nitro: {
-    unenv: {
-      external: ['https://cdn.skypack.dev/@mozilla/readability'],
-    },
-  },
 
   // https://hub.nuxt.com/docs/getting-started/installation#options
   hub: {
